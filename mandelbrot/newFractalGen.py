a9ebda5a4453aa4f4281b244712276555c1135cb
ahhhh6980 <49384256+ahhhh6980@users.noreply.github.com>
2020-09-03 14:59:45 -0400

    added a lil bit

--- mandelbrot/newFractalGen.py
+++ mandelbrot/newFractalGen.py
@@ -27,23 +27,28 @@ def split( ):
     return temp
 
 # Setup
-ratio = [16,9]
+ratio = [1,1]
 # Be careful, high resolutions take a LOT of ram
 # For 16:9, 5 gives you a 4800px X 2700px image, which may require up to or over 16gb of ram
-resolution = 5
+resolution = 10
 frame = [ round( 60*ratio[0]*resolution ), round( 60*ratio[1]*resolution ) ]
 img = Image.new( 'HSV', [ frame[0], frame[1] ], (100,0,360) )
 data = img.load()
 
 # Processing
+# WARNING: SET THIS APPROPRIATELY TO YOUR SYSTEM
 availableCores = 10
+colorExponent = 0.5
 
 #zoom = 0.4
-zoom = 1
+#zoom = 1
+zoom = 0.7
 pLimit = 2500
 l = pLimit
-position = complex(2,1.5)
-isMandelbrot = False
+#position = complex(2,1.5)
+#position = complex(-0.2045,0.2510)
+position = complex(-0.25,0.5)
+isMandelbrot = True
 
 # Computation function
 def computePixel( pixel ):
@@ -61,12 +66,12 @@ def computePixel( pixel ):
                     z = z**z + ( z / ( c + complex(0.001,0.001) ) )
                 if(str(z)=="(nan+nanj)"):
                     #print("YES")
-                    return [pixel,(scaleToRangeSimple(i,[0,l],[0,360],0.5))]
+                    return [pixel,[(scaleToRangeSimple(i,[0,l],[0,360],colorExponent)),i]]
                 
             except Exception:
                 #return ( mapRange( (pixel[0]/(pixel[1]+1))*i, [0,(frame[0]/frame[1])*l], [0,360] ) )
-                return [pixel,(scaleToRangeSimple(i,[0,l],[0,360],0.5))]
-    return [pixel,-1]
+                return [pixel,[(scaleToRangeSimple(i,[0,l],[0,360],colorExponent)),i]]
+    return [pixel,[-1,256]]
     #return -1"""
 
 def getSplit( i, s):
@@ -97,10 +102,10 @@ if __name__=="__main__":
     i = 0
     now = datetime.datetime.now()
 
-    print("STARTING COLOR ASSIGNMENT\n@"+getTime()+"\n")
+    print("\nSTARTING COLOR ASSIGNMENT\n@"+getTime())
     for e in r:
         i+=1
-        color = e[1]
+        color = e[1][0]
         x,y = e[0][0],e[0][1]
         if((i/(frame[0]*frame[1]) * 100)%int(100/consoleReadouts) == 0):
             now = datetime.datetime.now()
@@ -108,10 +113,14 @@ if __name__=="__main__":
         if(color==-1):
             data[x,y] = (0,0,0)
         else:
-            data[x,y] = (int(((color*(2))+90)%360), int(256/(1)), 256)
+            #int(e[1][1]/(1))
+            data[x,y] = (int(((color*(2))+(45*3))%360), 256, int((e[1][1])**1.5))
+    
+    now = datetime.datetime.now() 
+    print("\nSAVING IMAGE\n@"+getTime())
+    img.convert( mode="RGB" ).save( location+name+"_"+str(14)+'.png' )
 
     now = datetime.datetime.now()
     d = now - start
     print("\nFinished in "+str(d.total_seconds())+" Seconds!")
-    img.convert( mode="RGB" ).save( location+name+"_"+str(9)+'.png' )
     
